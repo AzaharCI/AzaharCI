1f76315564bd96031bcef7563683336190103f4e
Corte-Z <214324952+Corte-Z@users.noreply.github.com>
2025-06-05 15:22:10 -0400

    Fix release artifact names
    Signed-off-by: Corte-Z <214324952+Corte-Z@users.noreply.github.com>

--- .ci/pack.sh
+++ .ci/pack.sh
@@ -2,13 +2,13 @@
 
 mkdir -p artifacts
 cp linux-*/*AppImage* artifacts
-cp android*/app-release.apk artifacts/azahar-VERSION-universal.apk
+cp android*/app-release.apk artifacts/azahar-$VERSION-universal.apk
 
 for target in msys2 msvc
 do
-    zip -r artifacts/azahar-windows-$target-VERSION-portable.zip windows-$target-portable/*
+    zip -r artifacts/azahar-windows-$target-$VERSION-portable.zip windows-$target-portable/*
     if [ -d "windows-$target-installer" ]
     then
-        mv windows-$target-installer/*.exe artifacts/azahar-windows-$target-VERSION-installer.exe
+        mv windows-$target-installer/*.exe artifacts/azahar-windows-$target-$VERSION-installer.exe
     fi
 done
