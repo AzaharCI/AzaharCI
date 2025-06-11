1ec857f78ef329b3d77626cbddb9bd41550fc578
Corte-Z <214324952+Corte-Z@users.noreply.github.com>
2025-06-11 11:33:48 -0400

    remove explicit sharun download
    Signed-off-by: Corte-Z <214324952+Corte-Z@users.noreply.github.com>

--- .ci/linux/package.sh
+++ .ci/linux/package.sh
@@ -11,7 +11,6 @@ export BUILDDIR="$2"
 
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
-SHARUN="https://github.com/VHSgunzo/sharun/releases/download/v0.6.9/sharun-$ARCH"
 
 if [ "$ARCH" = 'x86_64' ]; then
 	if [ "$1" = 'v3' ]; then
@@ -88,9 +87,6 @@ xvfb-run -a -- ./lib4bin -p -v -e -s -k \
 if [ "$ARCH" = 'aarch64' ]; then
 	echo 'SHARUN_ALLOW_SYS_VKICD=1' > ./.env
 fi
-
-wget $SHARUN -O sharun
-chmod a+x sharun
 
 if [ "$ARCH" = 'aarch64' ]; then # allow using host vk for aarch64 given the sad situation
 	echo 'SHARUN_ALLOW_SYS_VKICD=1' >> ./.env
